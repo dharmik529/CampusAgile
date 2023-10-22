d955149c30587424f0767e934d1ab2359dd61d71
dharmikpatel <dharmik529@gmail.com>
2023-10-22 14:52:00 -0400

    sample classy classification

--- chatbot/chatbot/main.py
+++ chatbot/chatbot/main.py
@@ -1,8 +1,24 @@
-import torch
+import spacy
+
+data = {
+    "furniture": ["This text is about chairs.",
+               "Couches, benches and televisions.",
+               "I really need to get a new sofa."],
+    "kitchen": ["There also exist things like fridges.",
+                "I hope to be getting a new stove today.",
+                "Do you also have some ovens."]
+}
 
 def main():
-    x=torch.rand(5, 3)
-    print(x)
+    nlp = spacy.load('en_core_web_md')
+    nlp.add_pipe("classy_classification", 
+        config={
+            "data": data,
+            "model": "spacy"
+        }
+    )
+
+    print(nlp("I am looking for kitchen appliances.")._.cats)
 
 if __name__ == "__main__":
     main()
