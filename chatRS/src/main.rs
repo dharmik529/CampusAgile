663db695ed295b9f559639bf07e56363b9f84656
dharmikpatel <dharmik529@gmail.com>
2023-10-24 12:55:55 -0400

    added nlp libs, need to install libtorch

--- chatRS/src/main.rs
+++ chatRS/src/main.rs
@@ -1,3 +1,25 @@
+extern crate rust_stemmers;
+extern crate natural;
+
+use rust_stemmers::{ Algorithm, Stemmer };
+use natural::classifier::NaiveBayesClassifier;
+
 fn main() {
-    println!("Hello, world!");
+
+    // Create a stemmer for the english language
+    let en_stemmer = Stemmer::create(Algorithm::English);
+
+    // Stemm the word "fruitlessly"
+    // Please be aware that all algorithms expect their input to only contain lowercase characters.
+    // assert_eq!(en_stemmer.stem("fruitlessly"), "fruitless");
+    println!("{}", en_stemmer.stem("fruitlessly"));
+
+    let mut nbc = NaiveBayesClassifier::new();
+
+    nbc.train(STRING_TO_TRAIN, LABEL);
+    nbc.train(STRING_TO_TRAIN, LABEL);
+    nbc.train(STRING_TO_TRAIN, LABEL);
+    nbc.train(STRING_TO_TRAIN, LABEL);
+
+    nbc.guess(STRING_TO_GUESS); //returns a label with the highest probability
 }
