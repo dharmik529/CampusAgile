4beddd1def8934936e24ef40b23b99b3b335ed2c
dharmikpatel <dharmik529@gmail.com>
2023-10-24 17:14:21 -0400

    cargo to use torch

--- chatRS/src/main.rs
+++ chatRS/src/main.rs
@@ -1,8 +1,7 @@
 extern crate rust_stemmers;
-extern crate natural;
+extern crate rust_bert;
 
 use rust_stemmers::{ Algorithm, Stemmer };
-use natural::classifier::NaiveBayesClassifier;
 
 fn main() {
 
@@ -14,12 +13,17 @@ fn main() {
     // assert_eq!(en_stemmer.stem("fruitlessly"), "fruitless");
     println!("{}", en_stemmer.stem("fruitlessly"));
 
-    let mut nbc = NaiveBayesClassifier::new();
+    let sequence_classification_model = ZeroShotClassificationModel::new(Default::default())?;
 
-    nbc.train(STRING_TO_TRAIN, LABEL);
-    nbc.train(STRING_TO_TRAIN, LABEL);
-    nbc.train(STRING_TO_TRAIN, LABEL);
-    nbc.train(STRING_TO_TRAIN, LABEL);
+    let input_sentence = "Who are you voting for in 2020?";
+    let input_sequence_2 = "The prime minister has announced a stimulus package which was widely criticized by the opposition.";
+    let candidate_labels = &["politics", "public health", "economics", "sports"];
 
-    nbc.guess(STRING_TO_GUESS); //returns a label with the highest probability
+    let output = sequence_classification_model.predict_multilabel(
+        &[input_sentence, input_sequence_2],
+        candidate_labels,
+        None,
+        128,
+    );
+
 }
