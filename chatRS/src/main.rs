f0d59fc1fc45cd0f7495885737bfa617d11b80b5
dharmikpatel <dharmik529@gmail.com>
2023-10-26 13:03:15 -0400

    refactored

--- chatRS/src/main.rs
+++ chatRS/src/main.rs
@@ -14,15 +14,22 @@ fn main() {
     // assert_eq!(en_stemmer.stem("fruitlessly"), "fruitless");
     println!("{}", en_stemmer.stem("fruitlessly"));
 
-    let mut nbc = NaiveBayesClassifier::new();
+    let nbc = NaiveBayesClassifier::new();
 
-    nbc.train("This text is about chairs.", "Furniture");
-    nbc.train("Couches, benches and televisions.", "Furniture");
-    nbc.train("I really need to get a new sofa.", "Furniture");
-    nbc.train("There also exist things like fridges.", "Kitchen");
-    nbc.train("I hope to be getting a new stove today.", "Kitchen");
-    nbc.train("Do you also have some ovens.", "Kitchen");
+    let processor = trainer(nbc);
 
-    println!("{}", nbc.guess("I am looking for kitchen appliances.")); //returns a label with the highest probability
+    println!("{}", processor.guess("I am looking for a dresser.")); //returns a label with the highest probability
 
+}
+
+fn trainer(mut processor: NaiveBayesClassifier) -> NaiveBayesClassifier {
+
+    processor.train("This text is about chairs.", "Furniture");
+    processor.train("Couches, benches and televisions.", "Furniture");
+    processor.train("I really need to get a new sofa.", "Furniture");
+    processor.train("There also exist things like fridges.", "Kitchen");
+    processor.train("I hope to be getting a new stove today.", "Kitchen");
+    processor.train("Do you also have some ovens.", "Kitchen");
+
+    return processor;
 }
